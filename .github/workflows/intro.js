d151cfc490f4abd583a0003086c0f75b21eb77bc
ScriptBoy10 <miraclennamani110@gmail.com>
2026-06-14 17:14:35 +0100

    Add files via upload

---
+++ .github/workflows/intro.js
@@ -0,0 +1,5 @@
+const name = "Nnamani Miracle Oluebube";
+const age = 20;
+const programmingLanguage = "JavaScript";
+
+console.log(`Hello, my name is ${name}, I am ${age} years old, and I like to program in ${programmingLanguage}.`);
